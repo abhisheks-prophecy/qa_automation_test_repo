0016d0cca7f79c8633293054c31d6038850e5dce
abhisheks@prophecy.io <abhisheks@prophecy.io>
2025-01-23 05:50:42 +0000

    Add new pipeline and job for Automation_python_ReusableJobCluster

---
+++ test_1737610876767_Automation_python_ReusableJobCluster/Automation_python_ReusableJobCluster/pipelines/Automation_python_ReusableJobCluster_Pipeline/code/automation_python_reusablejobcluster_pipeline/pipeline.py
@@ -0,0 +1,33 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from automation_python_reusablejobcluster_pipeline.config.ConfigStore import *
+from automation_python_reusablejobcluster_pipeline.functions import *
+from prophecy.utils import *
+from automation_python_reusablejobcluster_pipeline.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    df_dataset_cust_in = dataset_cust_in(spark)
+    df_reformatted_customer_data = reformatted_customer_data(spark, df_dataset_cust_in)
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Automation_python_ReusableJobCluster_Pipeline")\
+                .getOrCreate()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/Automation_python_ReusableJobCluster_Pipeline")
+    registerUDFs(spark)
+    
+    MetricsCollector.instrument(
+        spark = spark,
+        pipelineId = "pipelines/Automation_python_ReusableJobCluster_Pipeline",
+        config = Config
+    )(
+        pipeline
+    )
+
+if __name__ == "__main__":
+    main()

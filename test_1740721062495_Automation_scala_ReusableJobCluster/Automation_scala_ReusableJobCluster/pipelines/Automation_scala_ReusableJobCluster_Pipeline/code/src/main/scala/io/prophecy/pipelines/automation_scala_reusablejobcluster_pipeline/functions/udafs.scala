d2927c9b40d61238e15d1f407fa478a6658c5c99
abhisheks@prophecy.io <abhisheks@prophecy.io>
2025-02-28 05:46:57 +0000

    Add new Scala pipeline and job for Automation_scala_ReusableJobCluster

---
+++ test_1740721062495_Automation_scala_ReusableJobCluster/Automation_scala_ReusableJobCluster/pipelines/Automation_scala_ReusableJobCluster_Pipeline/code/src/main/scala/io/prophecy/pipelines/automation_scala_reusablejobcluster_pipeline/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.automation_scala_reusablejobcluster_pipeline.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }

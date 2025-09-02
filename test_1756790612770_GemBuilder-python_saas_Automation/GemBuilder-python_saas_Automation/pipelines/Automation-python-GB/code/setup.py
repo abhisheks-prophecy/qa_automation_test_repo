dc294029c37274a147255b607617ce91f50435e9
abhisheks@prophecy.io <abhisheks@prophecy.io>
2025-09-02 05:31:58 +0000

    Add new pipeline 'Automation-python-GB', dataset 'GB-TestDataset', and gem 'CustomGem' for automation testing.

---
+++ test_1756790612770_GemBuilder-python_saas_Automation/GemBuilder-python_saas_Automation/pipelines/Automation-python-GB/code/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'Automation-python-GB',
+    version = '1.0',
+    packages = find_packages(include = ('automationpythongb*', )) + ['prophecy_config_instances.automationpythongb'],
+    package_dir = {'prophecy_config_instances.automationpythongb' : 'configs/resources/automationpythongb'},
+    package_data = {'prophecy_config_instances.automationpythongb' : ['*.json', '*.py', '*.conf']},
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==2.1.3'],
+    entry_points = {
+'console_scripts' : [
+'main = automationpythongb.pipeline:main'], },
+    data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
+    extras_require = {
+'test' : ['pytest', 'pytest-html', 'pytest-cov'], }
+)
